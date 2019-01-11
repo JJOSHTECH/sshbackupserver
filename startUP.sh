8abe098ea298ab391c4b714c10b7a7921861227c
JJOSHTECH <46561533+JJOSHTECH@users.noreply.github.com>
2019-01-11 09:35:49 +0100

    Update startUP.sh

--- startUP.sh
+++ startUP.sh
@@ -5,10 +5,12 @@ then
 	echo '0 * * * * rsync -vaze "ssh -p $PORT" root@$SERVER_ADDRESS:/var/backup/ /var/backup' >> cronjobs
 	crontab cronjobs
 	rm cronjobs
-	/usr/sbin/cron -f
+	/usr/sbin/cron -f &
 fi
 
 if [ "$TYPE" == "server" ] || [ "$TYPE" == "both" ]
 then
-	/usr/sbin/sshd -p 22 -D -e
+	/usr/sbin/sshd -p 22 -D -e &
 fi
+
+fg
